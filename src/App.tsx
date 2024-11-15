6e2a27fd2b075b1e2d61b6db22d19096282caa56
faisalrasheed1174 <faisalrasheed1174@gmail.com>
2024-11-15 22:49:33 +0300

    Removed the contact form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
-import { Hero } from './components/Hero';
-import { ProjectsGrid } from './components/ProjectsGrid';
-import { BlogSlider } from './components/BlogSlider';
-import { ContactForm } from './components/ContactForm';
-import { Footer } from './components/Footer';
-import { CodeRequestModal } from './components/CodeRequestModal';
+import React, {useState} from "react";
+import {Hero} from "./components/Hero";
+import {ProjectsGrid} from "./components/ProjectsGrid";
+import {BlogSlider} from "./components/BlogSlider";
+import {ContactForm} from "./components/ContactForm";
+import {Footer} from "./components/Footer";
+import {CodeRequestModal} from "./components/CodeRequestModal";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedProjectId, setSelectedProjectId] = useState<string>('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedProjectId, setSelectedProjectId] = useState<string>("");
 
-  const handleGetCode = (projectId: string) => {
-    setSelectedProjectId(projectId);
-    setIsModalOpen(true);
-  };
+    const handleGetCode = (projectId: string) => {
+        setSelectedProjectId(projectId);
+        setIsModalOpen(true);
+    };
 
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Hero />
-      <ProjectsGrid onGetCode={handleGetCode} />
-      <BlogSlider />
-      <ContactForm />
-      <Footer />
-      
-      <CodeRequestModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        projectId={selectedProjectId}
-      />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Hero />
+            <ProjectsGrid onGetCode={handleGetCode} />
+            <BlogSlider />
+            <Footer />
+
+            <CodeRequestModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                projectId={selectedProjectId}
+            />
+        </div>
+    );
 }
 
 export default App;
